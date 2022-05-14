3cec009318c419cdaa1dc2b98de305f49fe3524c
Egor Alisenok <egor.alisenok@gmail.com>
2022-05-14 17:29:44 +0300

    Task 1

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Reel : MonoBehaviour
 {
     [SerializeField] private RectTransform[] symbolOnReel;
-    private float exitPos = 195;
+    [SerializeField] private float exitPosition;
+    [SerializeField] private float startPositionY;
+    [SerializeField] private float spriteHeight;
+    [SerializeField] private Sprite[] sprites;
 
+    [SerializeField] private RectTransform mainCanvasRT;
+    private float mainCanvasScale;
+
+    private void Start()
+    {
+        mainCanvasScale = mainCanvasRT.lossyScale.y;
+    }
     private void Update()
     {
         foreach (var symbol in symbolOnReel)
         {
-            if (symbol.position.y <= exitPos)
+            if (symbol.position.y <= exitPosition * mainCanvasScale)
             {
-                Debug.Log(symbol.position.y);
-                var offset = symbol.position.y + 220 * 4;
-                var newPos = new Vector3(symbol.position.x, offset);
-                symbol.position = newPos;
+                MoveSymbolUp(symbol);
+                ChangeSymbolSprite(symbol);
             }
         }
+    }
+
+    public void ResetSymbolsPosition(float reelStartPositionY)
+    {
+        foreach(var symbol in symbolOnReel)
+        {
+            var symbolPos = symbol.localPosition;
+            var newPos = symbolPos.y + reelStartPositionY;
+            symbol.localPosition = new Vector3(symbolPos.x, newPos);
+        }
+    }
+
+    private void MoveSymbolUp(RectTransform symbol)
+    {
+        Debug.Log(symbol.position.y);
+        var offset = symbol.position.y + startPositionY * mainCanvasScale;
+        var newPos = new Vector3(symbol.position.x, offset);
+        symbol.position = newPos;
+    }
+    private void ChangeSymbolSprite(RectTransform symbol)
+    {
+        var random = Random.Range(0, sprites.Length);
+        symbol.GetComponent<Image>().sprite = sprites[random];
     }
 }
--- Assets/Scripts/ReelsRoll.cs
+++ Assets/Scripts/ReelsRoll.cs
@@ -2,32 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class ReelsRoll : MonoBehaviour
 {
-    [SerializeField] private RectTransform[] reels;
+    [SerializeField] private RectTransform[] reelsRT;
+    [SerializeField] private Reel[] reels;
     [SerializeField] private float delayStep;
     [SerializeField] private Ease startEase;
     [SerializeField] private Ease stopEase;
+    [SerializeField] private float boostDistance, linearDistance, stopDistance;
+    [SerializeField] private float boostDuration, linearDuration, stopDuration;
+    [SerializeField] private Button playButton;
+    private Dictionary<RectTransform, Reel> reelsDictionary;
+    private float reelStartPositionY;
+    private float thirdReelPosX;
+
+    public void Start()
+    {
+        reelsDictionary = new Dictionary<RectTransform, Reel>();
+        for (int i = 0; i< reelsRT.Length; i++)
+        {
+            reelsDictionary.Add(reelsRT[i], reels[i]);
+        }
+        thirdReelPosX = reelsRT[2].position.x;
+        reelStartPositionY = reelsRT[0].localPosition.y;
+    }
     public void ScrollStart()
     {
-        for (int i = 0; i < reels.Length; i++)
+        playButton.interactable = false;
+        for (int i = 0; i < reelsRT.Length; i++)
         {
-            var reel = reels[i];
+            var reelRT = reelsRT[i];
 
-            reel.DOAnchorPosY(-860, 1)
+            reelRT.DOAnchorPosY(boostDistance, boostDuration)
                 .SetDelay(i * delayStep)
                 .SetEase(startEase)
-                .OnComplete(() => ScrollLinear(reel));
+                .OnComplete(() => ScrollLinear(reelRT));
         }
     } 
 
-    private void ScrollLinear(RectTransform reel)
+    private void ScrollLinear(RectTransform reelRT)
     {
-        reel.DOAnchorPosY(-3520, 1).SetEase(Ease.Linear).OnComplete(() => ScrollStop(reel));
+        reelRT.DOAnchorPosY(linearDistance, linearDuration)
+            .SetEase(Ease.Linear)
+            .OnComplete(() => ScrollStop(reelRT));
     }
-    private void ScrollStop(RectTransform reel)
+    private void ScrollStop(RectTransform reelRT)
     {
-        reel.DOAnchorPosY(-4400, 1).SetEase(stopEase);
+        reelRT.DOAnchorPosY(stopDistance, stopDuration)
+            .SetEase(stopEase)
+            .OnComplete(() => {
+                PrepareReel(reelRT);
+                if (reelRT.position.x == thirdReelPosX)
+                {
+                    playButton.interactable = true;
+                }
+                });
+    }
+
+    private void PrepareReel(RectTransform reelRT)
+    {
+        var currentReelPosY = reelRT.localPosition.y;
+        reelRT.localPosition = new Vector3(reelRT.localPosition.x, reelStartPositionY);
+        reelsDictionary[reelRT].ResetSymbolsPosition(currentReelPosY);
     }
 }
